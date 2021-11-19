9f6fbe5d230de651763009536f72ab22bf8d149a
Abner Klug <abner.klug@2cloud.com.br>
2021-11-19 12:32:00 -0300

    atualização dos scripts

--- Externo/zabbix-novoagent5-externo-eqx_ubuntu.sh
+++ Externo/zabbix-novoagent5-externo-eqx_ubuntu.sh
@@ -3,7 +3,7 @@ cd /tmp
 wget https://repo.zabbix.com/zabbix/5.0/ubuntu/pool/main/z/zabbix-release/zabbix-release_5.0-1+$(lsb_release -sc)_all.deb
 dpkg -i zabbix-release_5.0-1+$(lsb_release -sc)_all.deb
 apt update
-
+apt-get -o Dpkg::Options::="--force-confnew" install zabbix-agent
 if [[ ! -e $dir ]]; then
   mkdir /home/scripts
   chown zabbix:zabbix -R /home/scripts
@@ -13,20 +13,22 @@ cd /home/scripts
 wget https://raw.githubusercontent.com/2cloudtecnologia/arquivos_publicos/main/sessions.sh
 chmod 744 *
 chown zabbix:zabbix *
-apt install -y zabbix-agent
 cd /etc/zabbix/zabbix_agentd.d
 wget https://raw.githubusercontent.com/2cloudtecnologia/arquivos_publicos/main/2cloud.conf
-echo Insira o hostname do agent:
-read hostname
+read -p "Tecle enter se o servidor for zabbix.2cloud.com.br, ou insira o IP/FQDN do proxy: `echo $'\n> '`" choice
+if [[ -z $choice ]]; then
+  server="zabbix.2cloud.com.br"
+else
+  server=$choice
+fi
+read -p "Insira o nome do agente  : `echo $'\n> '`" hostname
   sed -i "s/Hostname=Zabbix server/Hostname=$hostname/g" /etc/zabbix/zabbix_agentd.conf
-  sed -i 's/Server=127.0.0.1/Server=192.168.1.132/g' /etc/zabbix/zabbix_agentd.conf
-  sed -i 's/ServerActive=127.0.0.1/ServerActive=192.168.1.132/g' /etc/zabbix/zabbix_agentd.conf
-  sed -i 's/# HostMetadata=/HostMetadata=2cloud,equinix,interno,linux,ubuntu/g' /etc/zabbix/zabbix_agentd.conf
+  sed -i "s/Server=127.0.0.1/Server=$server/g" /etc/zabbix/zabbix_agentd.conf
+  sed -i "s/ServerActive=127.0.0.1/ServerActive=$server/g" /etc/zabbix/zabbix_agentd.conf
+  sed -i 's/# HostMetadata=/HostMetadata=cliente,ubuntu/g' /etc/zabbix/zabbix_agentd.conf
   echo "zabbix	ALL=(ALL)	NOPASSWD: ALL" >> /etc/sudoers
   service zabbix-agent restart
   systemctl enable zabbix-agent
   cat /etc/zabbix/zabbix_agentd.conf | grep Server=
   cat /etc/zabbix/zabbix_agentd.conf | grep ServerActive=
-  echo
-  echo !!! PRESSIONE "CTRL + C" PARA PARAR DE LER O LOG !!!
   tail -f /var/log/zabbix/zabbix_agentd.log
