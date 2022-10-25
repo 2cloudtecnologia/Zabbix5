62fef8ca5ed79ef4938b890c762c5d7c877a3ad7
Abner Klug <abnerfklug@gmail.com>
2022-10-25 14:36:22 -0300

    Update script ubuntu

--- Externo/zabbix-novoagent5-externo-eqx_ubuntu.sh
+++ Externo/zabbix-novoagent5-externo-eqx_ubuntu.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 cd /tmp
-wget https://repo.zabbix.com/zabbix/5.0/ubuntu/pool/main/z/zabbix-release/zabbix-release_5.0-1+$(lsb_release -sc)_all.deb
+wget --no-check-certificate https://repo.zabbix.com/zabbix/5.0/ubuntu/pool/main/z/zabbix-release/zabbix-release_5.0-1+$(lsb_release -sc)_all.deb
 dpkg -i zabbix-release_5.0-1+$(lsb_release -sc)_all.deb
 apt update
-apt-get -o Dpkg::Options::="--force-confnew" install zabbix-agent
-if [[ ! -e /home/scripts ]]; then
-  mkdir /home/scripts
-  chown zabbix:zabbix -R /home/scripts
-  chmod 744 -R /home/scripts
+apt install -y install zabbix-agent
+if [[ ! -e /home/2cloud_scripts ]]; then
+  mkdir /home/2cloud_scripts
+  chown zabbix:zabbix -R /home/2cloud_scripts
+  chmod 744 -R /home/2cloud_scripts
 fi
-cd /home/scripts
-wget https://raw.githubusercontent.com/2cloudtecnologia/arquivos_publicos/main/sessions.sh
+cd /home/2cloud_scripts
+wget --no-check-certificate https://raw.githubusercontent.com/2cloudtecnologia/arquivos_publicos/main/sessions.sh
 chmod 744 *
 chown zabbix:zabbix *
 cd /etc/zabbix/zabbix_agentd.d
 if [[ ! -f 2cloud.conf ]]; then
-  wget https://raw.githubusercontent.com/2cloudtecnologia/arquivos_publicos/main/2cloud.conf
+  wget --no-check-certificate https://raw.githubusercontent.com/2cloudtecnologia/arquivos_publicos/main/2cloud.conf
+  wget --no-check-certificate https://raw.githubusercontent.com/2cloudtecnologia/arquivos_publicos/main/os.conf -O os.conf
 fi
 read -p "Tecle enter se o servidor for zabbix.2cloud.com.br, ou insira o IP/FQDN do proxy: `echo $'\n> '`" choice
 if [[ -z $choice ]]; then
@@ -23,11 +24,12 @@ if [[ -z $choice ]]; then
 else
   server=$choice
 fi
-read -p "Insira o nome do agente  : `echo $'\n> '`" hostname
+read -p "Insira o nome do agente: `echo $'\n> '`" hostname
+read -p "Insira o hostmetada do agente: `echo $'\n> '`" hostmetada
   sed -i "s/Hostname=Zabbix server/Hostname=$hostname/g" /etc/zabbix/zabbix_agentd.conf
   sed -i "s/Server=127.0.0.1/Server=$server/g" /etc/zabbix/zabbix_agentd.conf
   sed -i "s/ServerActive=127.0.0.1/ServerActive=$server/g" /etc/zabbix/zabbix_agentd.conf
-  sed -i 's/# HostMetadata=/HostMetadata=cliente,ubuntu/g' /etc/zabbix/zabbix_agentd.conf
+  sed -i "s/# HostMetadata=/HostMetadata=$hostmetada/g" /etc/zabbix/zabbix_agentd.conf
   echo "zabbix	ALL=(ALL)	NOPASSWD: ALL" >> /etc/sudoers
   service zabbix-agent restart
   systemctl enable zabbix-agent
